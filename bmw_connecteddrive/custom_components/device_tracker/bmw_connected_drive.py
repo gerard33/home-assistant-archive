4a87bab2fd2ede12bd660fc462377fefd371f0a7
Gerard <gerard33@users.noreply.github.com>
2018-03-30 22:48:18 +0200

    Update bmw_connected_drive.py

--- bmw_connecteddrive/custom_components/device_tracker/bmw_connected_drive.py
+++ bmw_connecteddrive/custom_components/device_tracker/bmw_connected_drive.py
@@ -3,10 +3,11 @@
 For more details about this platform, please refer to the documentation at
 https://home-assistant.io/components/device_tracker.bmw_connected_drive/
 """
+import json
 import logging
+import requests
 
-from custom_components.bmw_connected_drive import DOMAIN \
-    as BMW_DOMAIN
+from custom_components.bmw_connected_drive import DOMAIN as BMW_DOMAIN
 from homeassistant.util import slugify
 
 DEPENDENCIES = ['bmw_connected_drive']
@@ -24,6 +25,9 @@ def setup_scanner(hass, config, see, discovery_info=None):
             tracker = BMWDeviceTracker(see, vehicle)
             account.add_update_listener(tracker.update)
             tracker.update()
+
+            account.async_add_to_group(
+                vehicle,  'device_tracker.{}'.format(tracker.dev_id))
     return True
 
 
@@ -34,18 +38,50 @@ class BMWDeviceTracker(object):
         """Initialize the Tracker."""
         self._see = see
         self.vehicle = vehicle
+        self.dev_id = slugify(self.vehicle.name)
 
     def update(self) -> None:
-        """Update the device info."""
-        dev_id = slugify(self.vehicle.modelName)
-        _LOGGER.debug('Updating %s', dev_id)
+        """Update the device info.
+        
+        Only update the state in home assistant if tracking in
+        the car is enabled.
+        """
+        if not self.vehicle.state.is_vehicle_tracking_enabled:
+            _LOGGER.debug('Tracking is disabled for vehicle %s',
+                          self.vehicle.modelName)
+            return
+        _LOGGER.debug('Updating %s', self.vehicle.modelName)
+        lat, lon = self.vehicle.state.gps_position
+        address = self.get_place(lat, lon)
+        heading = self.vehicle.state._attributes['position']['heading']
+        heading_rounded = 0
+        if heading:
+            heading_rounded = min(range(0, 361, 30),
+                                key=lambda x:abs(x-heading))
+            if heading_rounded == 360:
+                heading_rounded = 0
         attrs = {
-            'trackr_id': dev_id,
-            'id': dev_id,
-            'name': self.vehicle.modelName
+            'address': address,
+            'id': self.dev_id,
+            'heading': heading,
+            'heading_rounded': heading_rounded,
+            'last_update': self.vehicle.state.timestamp.replace(tzinfo=None),
+            'name': self.vehicle.name,
+            'vin': self.vehicle.vin
         }
         self._see(
-            dev_id=dev_id, host_name=self.vehicle.modelName,
+            dev_id=self.dev_id, host_name=self.vehicle.name,
             gps=self.vehicle.state.gps_position, attributes=attrs,
             icon='mdi:car'
         )
+    
+    def get_place(self, lat, lon):
+        """Get address info from Openstreetmap based on lat and lon."""
+        url = "https://nominatim.openstreetmap.org/reverse?format=json&"
+        url += "lat={}&lon={}".format(lat, lon)
+        response = requests.get(url)
+        data = response.json()
+        address = 'unknown'
+        if 'display_name' in data:
+            address = data['display_name']
+        return address
