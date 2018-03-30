974ccca528d494e6c068ba1022e5eb1fa0485cf1
Gerard <gerard33@users.noreply.github.com>
2018-03-30 22:49:43 +0200

    Update bmw_connected_drive.py

--- bmw_connecteddrive/custom_components/switch/bmw_connected_drive.py
+++ bmw_connecteddrive/custom_components/switch/bmw_connected_drive.py
@@ -32,7 +32,7 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
             for key, value in sorted(SWITCHES.items()):
                 device = BMWSwitch(account, vehicle, key, value[0], value[1])
                 devices.append(device)
-    add_devices(devices)
+    add_devices(devices, True)
 
 class BMWSwitch(SwitchDevice):
     """Representation of a BMW vehicle switch."""
@@ -42,9 +42,16 @@ class BMWSwitch(SwitchDevice):
         self._account = account
         self._vehicle = vehicle
         self._attribute = attribute
-        self._name = sensor_name
+        self._name = '{} {}'.format(self._vehicle.name, self._attribute)
+        self._unique_id = '{}-{}'.format(self._vehicle.vin, self._attribute)
+        self._sensor_name = sensor_name
         self._icon = icon
         self._state = None
+
+    @property
+    def unique_id(self):
+        """Return the unique ID of the binary sensor."""
+        return self._unique_id
 
     @property
     def name(self):
@@ -64,15 +71,15 @@ class BMWSwitch(SwitchDevice):
     @property
     def device_state_attributes(self):
         """Return the state attributes of the switch."""
-        vehicle_state = self._vehicle.state
         return {
-            'last_update': vehicle_state.timestamp,
-            'car': self._vehicle.modelName
+            'last_update': self._vehicle.state.timestamp.replace(tzinfo=None),
+            'car': self._vehicle.name,
+            'friendly_name': self._sensor_name
         }
 
     def turn_on(self, **kwargs):
         """Send the on command."""
-        _LOGGER.debug("%s: switching on %s", self._vehicle.modelName,
+        _LOGGER.debug("%s: switching on %s", self._vehicle.name,
                         self._name)
         if self._attribute == 'climate':
             self._vehicle.remote_services.trigger_remote_air_conditioning()
@@ -83,24 +90,27 @@ class BMWSwitch(SwitchDevice):
 
     def turn_off(self, **kwargs):
         """Send the off command."""
-        _LOGGER.debug("%s: switching off %s", self._vehicle.modelName,
+        _LOGGER.debug("%s: switching off %s", self._vehicle.name,
                         self._name)
         ### CAN CLIMATE BE PUT OFF?
 
     def update(self):
         """Update state of the switch."""
-        _LOGGER.info("%s: updating data for %s", self._vehicle.modelName,
+        _LOGGER.info("%s: updating data for %s", self._vehicle.name,
                      self._attribute)
 
         ### IS CLIMATE STATUS AVAILABLE?
         self._state = STATE_OFF
-        
-        self.schedule_update_ha_state()
+
+    def update_callback(self):
+        """Schedule a state update."""
+        self.schedule_update_ha_state(True)
 
     @asyncio.coroutine
     def async_added_to_hass(self):
         """Add callback after being added to hass.
+
         Show latest data after startup.
         """
-        self._account.add_update_listener(self.update)
-        yield from self.hass.async_add_job(self.update)
+        self._account.add_update_listener(self.update_callback)
+        self._account.async_add_to_group(self._vehicle, self.entity_id)
