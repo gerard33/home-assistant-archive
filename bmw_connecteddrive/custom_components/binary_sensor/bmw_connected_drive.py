793b9c10fa8f2f9a9aa4c86a31aceecd812092f0
Gerard <gerard33@users.noreply.github.com>
2018-04-23 23:36:41 +0200

    Add files via upload

--- bmw_connecteddrive/custom_components/binary_sensor/bmw_connected_drive.py
+++ bmw_connecteddrive/custom_components/binary_sensor/bmw_connected_drive.py
@@ -185,4 +185,3 @@ class BMWConnectedDriveSensor(BinarySensorDevice):
         Show latest data after startup.
         """
         self._account.add_update_listener(self.update_callback)
-        self._account.async_add_to_group(self._vehicle, self.entity_id)
